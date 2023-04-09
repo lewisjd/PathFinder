084bc99067e3fc72935aee889762b61e0321329d
Lewis <lewisdunning3@gmail.com>
2023-04-09 15:12:40 +0100

    P

--- javascript/script.js
+++ javascript/script.js
@@ -16,111 +16,112 @@ backBtn.addEventListener("click", undoLastAction);
 findShortestRouteBtn.addEventListener("click", findShortestRoute);
 
 function generateGrid() {
-  gridSize.width = parseInt(widthInput.value);
-  gridSize.height = parseInt(heightInput.value);
-  gridData = new Array(gridSize.width * gridSize.height).fill("white");
+    gridSize.width = parseInt(widthInput.value);
+    gridSize.height = parseInt(heightInput.value);
+    gridData = new Array(gridSize.width * gridSize.height).fill("white");
+  
+    gridContainer.style.width = `${gridSize.width * 30}px`;
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize.width}, 30px)`; // Add this line
+    gridContainer.innerHTML = "";
 
-  gridContainer.style.width = `${gridSize.width * 30}px`;
-  gridContainer.innerHTML = "";
+    for (let i = 0; i < gridSize.width * gridSize.height; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("grid-cell", "white");
+        cell.style.width = "30px";
+        cell.style.height = "30px";
 
-  for (let i = 0; i < gridSize.width * gridSize.height; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("grid-cell", "white");
-    cell.style.width = "30px";
-    cell.style.height = "30px";
+        cell.addEventListener("mousedown", cellMouseDown);
+        cell.addEventListener("mouseover", cellMouseOver);
+        cell.addEventListener("mouseup", cellMouseUp);
+        cell.addEventListener("dblclick", cellDoubleClick);
 
-    cell.addEventListener("mousedown", cellMouseDown);
-    cell.addEventListener("mouseover", cellMouseOver);
-    cell.addEventListener("mouseup", cellMouseUp);
-    cell.addEventListener("dblclick", cellDoubleClick);
-
-    gridContainer.appendChild(cell);
-  }
+        gridContainer.appendChild(cell);
+    }
 }
 
 function cellMouseDown(event) {
-  dragging = true;
-  dragColor = event.target.classList.contains("white") ? "black" : "white";
-  toggleCellColor(event.target);
-  event.target.classList.add("green-border");
-  actionHistory.push({ index: getIndex(event.target), type: "drag", color: dragColor });
+    dragging = true;
+    dragColor = event.target.classList.contains("white") ? "black" : "white";
+    toggleCellColor(event.target);
+    event.target.classList.add("green-border");
+    actionHistory.push({ index: getIndex(event.target), type: "drag", color: dragColor });
 }
 
 function cellMouseOver(event) {
-  if (dragging) {
-    toggleCellColor(event.target, dragColor);
-    event.target.classList.add("green-border");
-    actionHistory.push({ index: getIndex(event.target), type: "drag", color: dragColor });
-  }
+    if (dragging) {
+        toggleCellColor(event.target, dragColor);
+        event.target.classList.add("green-border");
+        actionHistory.push({ index: getIndex(event.target), type: "drag", color: dragColor });
+    }
 }
 
 function cellMouseUp(event) {
-  dragging = false;
-  gridContainer.querySelectorAll(".green-border").forEach(cell => cell.classList.remove("green-border"));
+    dragging = false;
+    gridContainer.querySelectorAll(".green-border").forEach(cell => cell.classList.remove("green-border"));
 }
 
 function cellDoubleClick(event) {
-  const cell = event.target;
-  const cellType = cell.getAttribute("data-cell-type");
+    const cell = event.target;
+    const cellType = cell.getAttribute("data-cell-type");
 
-  if (cellType === "start") {
-    cell.setAttribute("data-cell-type", "end");
-    cell.classList.remove("start");
-    cell.classList.add("end");
-    actionHistory.push({ index: getIndex(cell), type: "doubleClick", prevState: "start" });
-  } else if (cellType === "end") {
-    cell.setAttribute("data-cell-type", "");
-    cell.classList.remove("end");
-    cell.classList.add("white");
-    actionHistory.push({ index: getIndex(cell), type: "doubleClick", prevState: "end" });
-  } else {
-    cell.setAttribute("data-cell-type", "start");
-    cell.classList.remove("white");
-    cell.classList.add("start");
-    actionHistory.push({ index: getIndex(cell), type: "doubleClick", prevState: "" });
-  }
-}
-
-function toggleCellColor(cell, color) {
-  const oldColor = cell.classList.contains("white") ? "white" : "black";
-  const newColor = color || oldColor === "white" ? "black" : "white";
-  cell.classList.remove(oldColor);
-  cell.classList.add(newColor);
-  gridData[getIndex(cell)] = newColor;
-}
-
-function undoLastAction() {
-  if (actionHistory.length === 0) return;
-
-  const lastAction = actionHistory.pop();
-
-  const cell = gridContainer.children[lastAction.index];
-
-  switch (lastAction.type) {
-    case "drag":
-        toggleCellColor(cell);
-        break;
-    case "doubleClick":
-        if (lastAction.prevState === "start") {
-        cell.setAttribute("data-cell-type", "start");
-        cell.classList.remove("end");
-        cell.classList.add("start");
-        } else if (lastAction.prevState === "end") {
+    if (cellType === "start") {
         cell.setAttribute("data-cell-type", "end");
         cell.classList.remove("start");
         cell.classList.add("end");
-        } else {
+        actionHistory.push({ index: getIndex(cell), type: "doubleClick", prevState: "start" });
+    } else if (cellType === "end") {
         cell.setAttribute("data-cell-type", "");
-        cell.classList.remove("start");
+        cell.classList.remove("end");
         cell.classList.add("white");
-        }
-        break;
+        actionHistory.push({ index: getIndex(cell), type: "doubleClick", prevState: "end" });
+    } else {
+        cell.setAttribute("data-cell-type", "start");
+        cell.classList.remove("white");
+        cell.classList.add("start");
+        actionHistory.push({ index: getIndex(cell), type: "doubleClick", prevState: "" });
     }
+}
+
+function toggleCellColor(cell, color) {
+    const oldColor = cell.classList.contains("white") ? "white" : "black";
+    const newColor = color || oldColor === "white" ? "black" : "white";
+    cell.classList.remove(oldColor);
+    cell.classList.add(newColor);
+    gridData[getIndex(cell)] = newColor;
+}
+
+function undoLastAction() {
+    if (actionHistory.length === 0) return;
+
+    const lastAction = actionHistory.pop();
+
+    const cell = gridContainer.children[lastAction.index];
+
+    switch (lastAction.type) {
+        case "drag":
+            toggleCellColor(cell);
+            break;
+        case "doubleClick":
+            if (lastAction.prevState === "start") {
+            cell.setAttribute("data-cell-type", "start");
+            cell.classList.remove("end");
+            cell.classList.add("start");
+            } else if (lastAction.prevState === "end") {
+            cell.setAttribute("data-cell-type", "end");
+            cell.classList.remove("start");
+            cell.classList.add("end");
+            } else {
+            cell.setAttribute("data-cell-type", "");
+            cell.classList.remove("start");
+            cell.classList.add("white");
+            }
+            break;
+        }
 }
 
 function getIndex(cell) {
     return Array.from(gridContainer.children).indexOf(cell);
-    }
+}
     
 function findShortestRoute() {
     const startCell = Array.from(gridContainer.children).find(cell => cell.getAttribute("data-cell-type") === "start");
